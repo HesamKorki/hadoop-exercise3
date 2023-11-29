7831fdc19ae609de7860423f01f02634e683d34c
Hesam KORKI <hesam.korki@uni.lu>
2023-11-29 19:40:11 +0100

    finish 1c

--- BetterInvertedIndex.java
+++ BetterInvertedIndex.java
@@ -25,9 +25,10 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class BetterInvertedIndex {
 
-	public static class IdfMapper extends Mapper<Object, Text, Text, IntWritable> {
+	public static class IdfMapper extends Mapper<Object, Text, Text, Text> {
 
 		private Text word = new Text();
+        private Text count = new Text();
 		private Map<String, Integer> tokenCount = new HashMap<>();
 		private final String pattern = "<doc id=\"([^\"]+)\"[^>]+>([^<]+)</doc>";
 		private final Pattern regex = Pattern.compile(pattern);
@@ -53,47 +54,56 @@ public class BetterInvertedIndex {
 			}
 			for (String token : tokenCount.keySet()) {
 				word.set(token + "," + docId);
-				context.write(word, new IntWritable(tokenCount.get(token)));
+                count.set(String.valueOf(tokenCount.get(token)));
+				context.write(word, count);
 			}
 		}
 	}
 
-	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, Text> {
+	public static class IntSumReducer extends Reducer<Text, Text, Text, Text> {
 
 		private Text result = new Text();
+        private String prevToken;
+        private List<String> postingsList;
 
         @Override
         public void setup(Context context) throws IOException, InterruptedException {
-            String prevToken = "";
-            List<String> postingsList = new ArrayList<>();
+            prevToken = "";
+            postingsList = new ArrayList<>();
         }
 		
 		@Override
-		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+		public void reduce(Text key, Iterable<Text> values, Context context)
 				throws IOException, InterruptedException {
-				
 			
-			for (IntWritable val : values) {
-				postingsList.add(posting.toString());
+            String[] key_content = key.toString().split(",");
+            String token = key_content[0];
+            String docId = key_content[1];
+
+            // If the token is different from the previous token, then emit the previous token's postings list
+            // and reset the postings list
+            if (!token.equals(prevToken)) {
+                if (!prevToken.equals("")) {
+                    result.set(String.join(", ", postingsList));
+                    context.write(new Text(prevToken), result);
+                    postingsList.clear();
+                }
+            }
+			
+			for (Text val : values) {
+				postingsList.add(docId + ":" + val.toString());
 			}
-			// Custom comparator for sorting based on ID
-			Comparator<String> idComparator = new Comparator<String>() {
-    			@Override
-    			public int compare(String s1, String s2) {
-        			// Extract IDs from strings (assuming "id:count" format)
-					String id1 = s1.split(":")[0];
-					String id2 = s2.split(":")[0];
-					// Compare IDs as strings
-					return id1.compareTo(id2);
-    			}
-			};
-			// Sort postings list
-			Collections.sort(postingsList, idComparator);
-			result.set(String.join(", ", postingsList));
-			context.write(key, result);
+            prevToken = token;
 		}
 
-	}
+        @Override
+        public void cleanup(Context context) throws IOException, InterruptedException {
+            // Emit the last token's postings list
+            result.set(String.join(", ", postingsList));
+            context.write(new Text(prevToken), result);
+
+	    }
+    }
 
 	public static void main(String[] args) throws Exception {
 
@@ -102,9 +112,8 @@ public class BetterInvertedIndex {
 		job.setJar("BetterInvertedIndex.jar");
 
 		job.setMapperClass(IdfMapper.class);
-		job.setCombinerClass(IntSumReducer.class); // enable to use 'local aggregation'
 		job.setReducerClass(IntSumReducer.class);
-
+        
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 
@@ -113,4 +122,5 @@ public class BetterInvertedIndex {
 
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
+    
 }
